5d50a0438d9d44c4fe40149c98c280a24c1d46c3
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2020-02-19 12:50:30 +0100

    add the `loadNUntil()` method
    This method runs n times `doUntil()` and returns each `Result`  stored  in
    a `ResultMap` associated with the appropriate initial terms. Moreover, the
    latter works asynchronously in order to increase performance.
    The documentation comments have been updated. Besides, an alias `mustinit`
    has been created to prevent some redundancies:  it adds a warning  telling
    that the given `vec_t` variable must have a size different from zero.
    `std::shared_ptr` has been replaced by an alias `Ref` to make the code
    more readable.
    An `Aggregater` has been added to allow the use of aggregate
    initialization for smart pointers.
    
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- src/core/Sequence.cpp
+++ src/core/Sequence.cpp
@@ -18,12 +18,15 @@
 
 #include "core/Sequence.hpp"
 
+#include <future>
+#include <iostream>
+
 namespace sequence
 {
-    int64_t Sequence::at(vec_t::size_type n) const
+    int64_t Sequence::at(const vec_t::size_type n, const vec_t& uz) const
     {
         int64_t ret;
-        auto uzSize = m_uz.size();
+        const auto uzSize = uz.size();
 
         if (uzSize == 0) {
             throw std::invalid_argument("Sequence::at(): When you create a "
@@ -33,11 +36,11 @@ namespace sequence
         }
 
         if (n < uzSize) {
-            ret = m_uz.at(n);
+            ret = uz.at(n);
         } else {
             vec_t vec;
             for (vec_t::size_type i = 1; i <= uzSize; ++i)
-                vec.push_back(at(n - i));
+                vec.push_back(at(n - i, uz));
 
             ret = m_seq(vec);
         }
@@ -45,13 +48,41 @@ namespace sequence
         return ret;
     }
 
-    Sequence::vec_t::size_type Sequence::doUntil(int64_t value) const
+    Ref<Sequence::Result> Sequence::doUntil(const int64_t value,
+                                            const vec_t uz) const
     {
-        vec_t::size_type stepCounter = 0;
+        vec_t::size_type cycle = 0;
+        int64_t maxTerm = at(0);
 
-        while (at(stepCounter) != value)
-            ++stepCounter;
+        for (auto term = maxTerm; term != value; term = at(++cycle, uz)) {
+            if (term > maxTerm)
+                maxTerm = term;
+        }
 
-        return stepCounter;
+        return std::make_shared<Aggregater<Result>>(cycle, maxTerm);
+    }
+
+    Ref<Sequence::ResultMap> Sequence::loadNUntil(const uint8_t n,
+                                                  const int64_t value,
+                                                  const uint8_t step) const
+    {
+        vec_t uz = m_uz;
+
+        using doUntil_t = Ref<Result> (Sequence::*)(int64_t,
+                                                    const vec_t) const;
+        std::map<vec_t, std::future<Ref<Result>>> map;
+        for (uint8_t i = 0; i < n; ++i) {
+            map[uz] = std::async(static_cast<doUntil_t>(&Sequence::doUntil),
+                                 this, value, uz);
+
+            std::for_each(uz.begin(), uz.end(),
+                          [=](int64_t& i) { i += step; });
+        }
+
+        auto ret = std::make_shared<ResultMap>();
+        for (auto& [key, i] : map)
+            (*ret)[key] = i.get();
+
+        return ret;
     }
 }
--- src/core/Sequence.hpp
+++ src/core/Sequence.hpp
@@ -19,7 +19,12 @@
 #ifndef SYRACUSE_SEQUENCE_HPP
 #define SYRACUSE_SEQUENCE_HPP
 
+#include "core/core.hpp"
+
 #include <functional>
+#include <map>
+
+using namespace core;
 
 /**
  * @brief Namespace providing some utilities about sequences.
@@ -40,8 +45,8 @@ namespace sequence
         /**
          * @brief Type reprensenting a vector of terms.
          *
-         * This type is used by the recurrence relation for defining the initial
-         * terms.
+         * This type is used by the recurrence relation for defining the
+         * initial terms.
          */
         using vec_t = std::vector<int64_t>;
         /**
@@ -58,10 +63,36 @@ namespace sequence
          *     return un_[1] + un_[0];
          * };
          * ```
-         *
-         * @see vec_t
          */
         using seq_t = std::function<int64_t(const vec_t&)>;
+
+        /**
+         * @struct Result core/Sequence.hpp
+         * @brief Structure used for containing the result of the `doUntil()`
+         * methods.
+         *
+         * @see doUntil(const int64_t value, const vec_t uz) const
+         * @see doUntil(const int64_t value) const
+         */
+        struct Result
+        {
+            /**
+             * @var vec_t::size_type cycleLen
+             * The computed length of the cycle.
+             */
+            vec_t::size_type cycleLen;
+            /**
+             * @var int64_t maxTerm
+             * The maximum term found during the process.
+             */
+            int64_t maxTerm;
+        };
+
+        /**
+         * @brief A map storing where each \p Result pointer is associated
+         * with the given initial terms.
+         */
+        using ResultMap = std::map<vec_t, Ref<Result>>;
     public:
         /**
          * @brief Construct an object from a recurrence relation and set the
@@ -81,8 +112,6 @@ namespace sequence
          *
          * @param uz  list of the initial terms
          * @param seq the recurrence relation
-         *
-         * @see seq_t
          */
         Sequence(const vec_t& uz, const seq_t& seq)
             : m_uz(uz), m_seq(seq) {}
@@ -92,7 +121,7 @@ namespace sequence
          *
          * @param seq the recurrence relation
          *
-         * @see Sequence(const vec_t& uz, const seq_t& s)
+         * @see Sequence(const vec_t& uz, const seq_t& seq)
          */
         Sequence(const seq_t& seq)
             : Sequence({}, seq) {}
@@ -117,25 +146,59 @@ namespace sequence
         inline Sequence& withUz(const vec_t& uz);
 
         /**
-         * @brief Get the term of the corresponding rank.
+         * @brief Get the term of the corresponding rank with the given
+         * initial terms.
          *
          * This method is the equivalent of \f$u_n\f$.
          *
-         * @warning
-         * To use this method, \p m_uz must be initialized, or otherwise an
-         * `std::invalid_argument` will be thrown.
+         * @mustinit{uz}
          *
-         * @param n corresponding rank
-         * @return  nth term
+         * @param n  the corresponding rank
+         * @param uz the initial terms
+         * @return   the nth term
          */
-        int64_t at(vec_t::size_type n) const;
+        int64_t at(const vec_t::size_type n, const vec_t& uz) const;
         /**
-         * @brief Get the cycle length.
+         * @brief Get the term of the corresponding rank.
+         *
+         * @mustinit{m_uz}
+         *
+         * @param n the corresponding rank
+         * @return  the nth term
+         *
+         * @see at(const vec_t::size_type n, const vec_t& uz) const
+         */
+        inline int64_t at(const vec_t::size_type n) const;
+        /**
+         * @brief Run the sequence until some value with the given initial
+         * terms.
+         *
+         * @mustinit{uz}
          *
          * @param value run the sequence until
-         * @return      the number of steps before arriving at \p value
+         * @param uz    the initial terms
+         * @return      some statistics
          */
-        vec_t::size_type doUntil(int64_t value) const;
+        Ref<Result> doUntil(const int64_t value, const vec_t uz) const;
+        /**
+         * @brief Run the sequence until some value.
+         *
+         * @mustinit{m_uz}
+         *
+         * @param value run the sequence until
+         * @return      some statistics
+         */
+        inline Ref<Result> doUntil(const int64_t value) const;
+        /**
+         * @brief Run `doUntil()` several times asynchronously.
+         *
+         * @param n     run `doUntil()` \p n times
+         * @param value run the sequence until
+         * @param step  the step incrementing the initial terms
+         * @return      a pointer to a map where each \p Result is associated
+         *              with the initial terms
+         */
+        Ref<ResultMap> loadNUntil(const uint8_t n, const int64_t value, const uint8_t step = 1) const;
     private:
         vec_t m_uz;
         seq_t m_seq;
@@ -151,6 +214,16 @@ namespace sequence
     {
         m_uz = uz;
         return *this;
+    }
+
+    inline int64_t Sequence::at(vec_t::size_type n) const
+    {
+        return at(n, m_uz);
+    }
+
+    inline Ref<Sequence::Result> Sequence::doUntil(const int64_t value) const
+    {
+        return doUntil(value, m_uz);
     }
 }
 
